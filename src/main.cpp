e52112bf2c257aea8f2913cb4193b7ded1dcaafa
nicolas.indjein <nicolas.indjein@epita.fr>
2022-05-13 17:09:00 +0200

    feat: improve read png

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #include "CPU/harris.hpp"
 
-png_bytepp read_png(const std::string file_name)
+png_bytepp read_png(const std::string file_name, int *width, int *height)
 {
     FILE *fp = fopen(file_name.c_str(), "rb");
     if (fp == NULL)
@@ -13,8 +13,40 @@ png_bytepp read_png(const std::string file_name)
     png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     auto info_ptr = png_create_info_struct(png_ptr);
     png_init_io(png_ptr, fp);
-    png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    png_read_info(png_ptr, info_ptr);
+
+    *width = png_get_image_width(png_ptr, info_ptr);
+    *height = png_get_image_height(png_ptr, info_ptr);
+    int color_type = png_get_color_type(png_ptr, info_ptr);
+    int bit_depth  = png_get_bit_depth(png_ptr, info_ptr);
+
+    // Format dark magic
+    if(bit_depth == 16)
+        png_set_strip_16(png_ptr);
+
+    if(color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(png_ptr);
+
+    if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png_ptr);
+
+    if(png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png_ptr);
+
+    if(color_type == PNG_COLOR_TYPE_RGB ||
+        color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
+
+    if(color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(png_ptr);
+
+    png_read_update_info(png_ptr, info_ptr);
+
     auto row_pointers = png_get_rows(png_ptr, info_ptr);
+
     png_destroy_read_struct(&png_ptr, NULL, NULL); 
     fclose(fp);
 
@@ -34,7 +66,9 @@ int main(int argc, char** argv)
 
     CLI11_PARSE(app, argc, argv);
 
-    png_bytepp image = read_png(inputFile);
+    int imageWidth;
+    int imageHeight;
+    png_bytepp image = read_png(inputFile, &imageWidth, &imageHeight);
     if(image == nullptr)
     {
         std::cout << "Could not read the image: " << inputFile << std::endl;
